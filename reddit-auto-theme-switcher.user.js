dcbf5774c14e574a95d5a1119d4b00809e441712
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2022-05-13 00:54:27 +0300

    fix menu not closing, small improvements

--- reddit-auto-theme-switcher.user.js
+++ reddit-auto-theme-switcher.user.js
@@ -6,28 +6,33 @@
 // @match           https://www.reddit.com/*
 // @name            Reddit auto theme switcher
 // @updateURL       https://github.com/danielvigaru/reddit-auto-theme-switcher.user.js/raw/master/reddit-auto-theme-switcher.user.js
-// @version         1.0.1
+// @version         1.0.2
 // ==/UserScript==
 
 function isLightTheme() {
     const somePartOfTheSiteIdk = document.querySelector("._1VP69d9lk-Wk9zokOaylL");
     const currentBgColor = getComputedStyle(somePartOfTheSiteIdk).backgroundColor;
-    if (currentBgColor === "rgb(255, 255, 255)") return true;
-    return false;
+    return currentBgColor === "rgb(255, 255, 255)";
+}
+
+function toggleMenu() {
+    const dropdown = document.getElementById("USER_DROPDOWN_ID");
+    dropdown.click();
+}
+
+function toggleTheme() {
+    toggleMenu();
+    const darkModeToggle = document.querySelectorAll("._3kUvbpMbR21zJBboDdBH7D")[1];
+    darkModeToggle.click();
+    setTimeout(toggleMenu, 500);
 }
 
 window.onload = () => {
     if (isLightTheme() && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        const dropdown = document.getElementById("USER_DROPDOWN_ID");
-        dropdown.click();
-        const darkModeToggle = document.querySelectorAll("._3kUvbpMbR21zJBboDdBH7D")[1];
-        darkModeToggle.click();
+        toggleTheme();
     }
 
     if (!isLightTheme() && window.matchMedia("(prefers-color-scheme: light)").matches) {
-        const dropdown = document.getElementById("USER_DROPDOWN_ID");
-        dropdown.click();
-        const darkModeToggle = document.querySelectorAll("._3kUvbpMbR21zJBboDdBH7D")[1];
-        darkModeToggle.click();
+        toggleTheme();
     }
 };
