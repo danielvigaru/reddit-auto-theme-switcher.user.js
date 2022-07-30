60b978c6b932bf56d9b540fada3028e9af7bc752
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2022-07-30 23:03:08 +0300

    replace with self invoking function

--- reddit-auto-theme-switcher.user.js
+++ reddit-auto-theme-switcher.user.js
@@ -6,33 +6,35 @@
 // @match           https://www.reddit.com/*
 // @name            Reddit auto theme switcher
 // @updateURL       https://github.com/danielvigaru/reddit-auto-theme-switcher.user.js/raw/master/reddit-auto-theme-switcher.user.js
-// @version         1.0.2
+// @version         1.1.0
 // ==/UserScript==
 
-function isLightTheme() {
-    const somePartOfTheSiteIdk = document.querySelector("._1VP69d9lk-Wk9zokOaylL");
-    const currentBgColor = getComputedStyle(somePartOfTheSiteIdk).backgroundColor;
-    return currentBgColor === "rgb(255, 255, 255)";
-}
-
-function toggleMenu() {
-    const dropdown = document.getElementById("USER_DROPDOWN_ID");
-    dropdown.click();
-}
-
-function toggleTheme() {
-    toggleMenu();
-    const darkModeToggle = document.querySelectorAll("._3kUvbpMbR21zJBboDdBH7D")[1];
-    darkModeToggle.click();
-    setTimeout(toggleMenu, 500);
-}
-
-window.onload = () => {
-    if (isLightTheme() && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        toggleTheme();
+(function () {
+    function isLightTheme() {
+        const somePartOfTheSiteIdk = document.querySelector("._1VP69d9lk-Wk9zokOaylL");
+        const currentBgColor = getComputedStyle(somePartOfTheSiteIdk).backgroundColor;
+        return currentBgColor === "rgb(255, 255, 255)";
     }
 
-    if (!isLightTheme() && window.matchMedia("(prefers-color-scheme: light)").matches) {
-        toggleTheme();
+    function toggleMenu() {
+        const dropdown = document.getElementById("USER_DROPDOWN_ID");
+        dropdown.click();
     }
-};
+
+    function toggleTheme() {
+        toggleMenu();
+        const darkModeToggle = document.querySelectorAll("._3kUvbpMbR21zJBboDdBH7D")[1];
+        darkModeToggle.click();
+        setTimeout(toggleMenu, 500);
+    }
+
+    setInterval(() => {
+        if (isLightTheme() && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            toggleTheme();
+        }
+
+        if (!isLightTheme() && window.matchMedia("(prefers-color-scheme: light)").matches) {
+            toggleTheme();
+        }
+    }, 2000);
+})();
